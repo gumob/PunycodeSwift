4c455cccc391337379ddcbe9bcb2d65f3537dc79
twodayslate <zac@gorak.us>
2020-02-16 15:40:17 -0500

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Punycode",
+    products: [
+        .library(
+            name: "PunnyCodeSwift",
+            targets: ["Punycode"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Punycode",
+            dependencies: [],
+            path: "Source"),
+        .testTarget(
+            name: "PunnycodeSwiftTests",
+            dependencies: ["Punycode"],
+            path: "Tests")
+    ]
+)
