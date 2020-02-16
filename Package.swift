d2fb1af8000abfe614c69cc60fecd6e46f45b3d8
twodayslate <zac@gorak.us>
2020-02-16 15:43:48 -0500

    Fix houndci issues

--- Package.swift
+++ Package.swift
@@ -8,11 +8,9 @@ let package = Package(
     products: [
         .library(
             name: "PunnyCodeSwift",
-            targets: ["Punycode"]),
+            targets: ["Punycode"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Punycode",
             dependencies: [],
