ae98815f4d0fea70f8b95cb1259c75edbc2559bc
gumob <gumob.dev@gmail.com>
2024-08-22 17:49:23 +0900

    ci: Introduce fzf for selecting fastlane commands in fastlane.sh script, improve user experience by providing a list of available commands and allowing easy selection using fzf.

--- fastlane.sh
+++ fastlane.sh
@@ -1,12 +1,30 @@
-#!/bin/bash
+#!/bin/zsh
 
-executeCommand () {
-	cmd=$1
-    echo
-	echo "$ $cmd"
-    echo
-	eval $cmd
-	echo
-}
+local option_list=(
+	"bundle exec fastlane ios prebuild"
+	"bundle exec fastlane ios set_version"
+	"bundle exec fastlane ios bump_version"
+	"bundle exec fastlane ios tests"
+	"bundle exec fastlane ios lint_spm"
+	"bundle exec fastlane ios build_carthage"
+	"bundle exec fastlane ios lint_cocoapods"
+	"bundle exec fastlane ios push_cocoapods"
+)
 
-executeCommand "bundle exec fastlane"
+if ! command -v fzf &> /dev/null; then
+    tput setaf 1; echo "fzf is not installed."; tput sgr0
+    exit 1
+fi
+
+
+local command=$(printf "%s\n" "${option_list[@]}" | fzf --ansi --prompt="Select a fastlane command > ")
+
+if [ -z "$command" ]; then
+	echo "No command selected"
+	exit 1
+fi
+
+echo "\n$command\n"
+eval $command
+
+exit 0
